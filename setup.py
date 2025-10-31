9cb55a2a8e0e3cc7f6e10a50dda63f1fffe3e0b4
Asanka Wijesooriya <ramwijesooriya@gmail.com>
2025-10-31 16:56:48 +0530

    medical chat bot project completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Asanka Wijesooriya",
+    author_email="ramwijesooriya@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
